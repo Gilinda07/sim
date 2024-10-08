08caa42067f369f8848375aa2f68415bb0f6b38d
Gilinda07 <167218371+Gilinda07@users.noreply.github.com>
2024-10-08 12:19:40 -0300

    Add files via upload

---
+++ pergnta.js
@@ -0,0 +1,24 @@
+criaCartao(
+    'Felipe',
+    'O Felipe é homossexual?',
+    'Sim.'
+)
+
+criaCartao(
+    'Mateus',
+    'O Mateus é um leprechaun?',
+    'Sim.'
+)
+
+criaCartao(
+    'Teixeira',
+    'A Teixeira gosta do Guilherme?',
+    'Sim.'
+)
+
+criaCartao(
+    'Giovana',
+    'A Giovana é linda',
+    'Sim.'
+)
+
