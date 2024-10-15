7edee98880cb46f9d7c6e3451a0e6856a361ebe2
Gilinda07 <167218371+Gilinda07@users.noreply.github.com>
2024-10-15 12:20:55 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Felipe',
+    'O Felipe é homossexual?',
+    'Sim.'
+)
+
+criaCartao(
+    'Mateus',
+    'O Mateus é um leprechaun?',
+    'Sim.'
+)
+
+criaCartao(
+    'Teixeira',
+    'A Teixeira gosta do Guilherme?',
+    'Sim.'
+)
+
+criaCartao(
+    'Giovana',
+    'A Giovana é linda?',
+    'Sim.'
+)
+
+criaCartao(
+    'Vagnes',
+    'Vai pagar um lanche pra mim?',
+    'Sim.'
+)
+
+criaCartao(
+    'Erik',
+    'É garoto de programa?',
+    'Sim.'
+)
